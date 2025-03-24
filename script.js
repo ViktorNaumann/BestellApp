09c3e7adfcdb429efc635d1e23938628a6aa7e86
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-03-24 11:33:43 +0100

    basket update

--- script.js
+++ script.js
@@ -1,24 +1,76 @@
-function menuRender() { // Erstellt eine Funktion renderBooks(), die die Bücher im HTML anzeigt.
-
-    for (let i = 0; i < Steaks.length; i++) { // Eine for-Schleife läuft über alle Bücher im books-Array.
-        let element = document.getElementById('content'); // Holt das HTML-Element mit der ID content, um dort die Bücher einzufügen.
-        element.innerHTML += templateHtmlRender(i); // Fügt das Buch mit Hilfe der templateHtmlRender(i)-Funktion in das HTML ein.
+function menuRender() {
+    for (let i = 0; i < Steaks.length; i++) {
+        let element = document.getElementById('content');
+        element.innerHTML += templateSteakRender(i);
     }
+    
+    for (let y = 0; y < fish.length; y++) {
+        let element = document.getElementById('content1');
+        element.innerHTML += templateFishRender(y);
+    } 
 }
 
-function templateHtmlRender(i) {
+function warenkorbRender() {
+    let element = document.getElementById(`warenkorb_content`);
+    element.innerHTML = "";
+    for (let j = 0; j < warenkorb.length; j++) {
+        element.innerHTML += templateHtmlRenderWarenkorb(j);
+    }
+
+}
+
+function templateSteakRender(i) {
     return /*html*/`
         <div class="menu_steaks">
             <div class="steak_name">
                 <h2>${Steaks[i].name}</h2>
-                <img id="addMenu" class="plus_logo" src="./img/plus.png" alt="">
+                <img onclick="addMenu(${i})" class="plus_logo" src="./img/plus.png" alt="">
             </div>
             <div class="steak_info">
-                <h3>Herkunft ${Steaks[i].info}</h3>
+                <h3>Herkunft: ${Steaks[i].info}</h3>
             </div>
-            <div>
+            <div class="price_weight">
                 <h4>${Steaks[i].amount} x ${Steaks[i].weight} g - ${Steaks[i].price.toFixed(2)} €</h4>
             </div>
         </div>
     `
+}
+
+function templateFishRender(i) {
+    return /*html*/`
+        <div class="menu_steaks">
+            <div class="steak_name">
+                <h2>${fish[i].name}</h2>
+                <img onclick="addMenu(${i})" class="plus_logo" src="./img/plus.png" alt="">
+            </div>
+            <div class="steak_info">
+                <h3>Herkunft: ${fish[i].info}</h3>
+            </div>
+            <div class="price_weight">
+                <h4>${fish[i].amount} x ${fish[i].weight} g - ${fish[i].price.toFixed(2)} €</h4>
+            </div>
+        </div>
+    `
+}
+
+function templateHtmlRenderWarenkorb(j){
+    return /*html*/`
+        <div>
+            ${warenkorb[j].amount}
+            ${warenkorb[j].name}
+            ${warenkorb[j].price.toFixed(2)} €
+        </div>
+    `
+}
+
+
+function addMenu(i) {
+    warenkorb.push ({
+        amount: Steaks[i].amount,
+        name: Steaks[i].name,
+        price: Steaks[i].price
+    });
+    
+    warenkorbRender();  
+    
 }
