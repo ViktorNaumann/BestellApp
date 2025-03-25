179e515b71b34260f3949f6a1a24fed4bc649010
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-03-25 10:47:49 +0100

    delete Amount

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function templateSteakRender(i) {
         <div class="menu_steaks">
             <div class="steak_name">
                 <h2>${Steaks[i].name}</h2>
-                <img onclick="addMenu('${Steaks[i].name}', ${Steaks[i].price})" class="plus_logo" src="./img/plus.png" alt="">
+                <img onclick="addMenu('${Steaks[i].name}', ${Steaks[i].price})" class="plus_logo" src="./img/plus.png" alt="addMenu">
             </div>
             <div class="steak_info">
                 <h3>Herkunft: ${Steaks[i].info}</h3>
@@ -34,13 +34,37 @@ function templateSteakRender(i) {
 
 function templateHtmlRenderWarenkorb(j) {
     return /*html*/`
-
-        <div>
-            <p><p>-</p>${warenkorbAmounts[j]} x ${warenkorbMenus[j]}><p>+</p></p>
-            <p>${(warenkorbPrices[j] * warenkorbAmounts[j]).toFixed(2)} €</p>
+        <div><p>${warenkorbMenus[j]}</p></div>
+        <div class="menus_price">
+            <p><p onclick="deleteAmount(${j})">-</p> ${warenkorbAmounts[j]} <p onclick="addAmount(${j})">+</p></p>
+            <ins>${(warenkorbPrices[j] * warenkorbAmounts[j]).toFixed(2)} €</ins>
+            <img class="delete_Menu" onclick="deleteAll(${j})" src="./img/mülleimer.png" height="40px" alt="deleteMenu">
         </div>
     `
 }
+
+function deleteAmount(j) {
+    if (warenkorbAmounts[j] > 1) {
+        warenkorbAmounts[j]--;
+    } else {
+        deleteAll(j);
+    }
+    warenkorbRender();
+}
+
+function addAmount(j) {
+    warenkorbAmounts[j]++;
+    warenkorbRender();
+}
+
+function deleteAll(j) {
+    warenkorbMenus.splice(j, 1);
+    warenkorbPrices.splice(j, 1);
+    warenkorbAmounts.splice(j, 1);
+    warenkorbRender();
+}
+
+
 
 function getMenuIndex(menuName) {
     return warenkorbMenus.indexOf(menuName);
